ff55504318f198f779b76ece4fda955a7753344d
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-26 12:49:19 -0700

    Agregar archivos de proyecto.

---
+++ EquipoBeisbol/Jugador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipoBeisbol
+{
+    class Jugador
+    {
+        public string Nombre { get; set; }
+        public string Numero { get; set; }
+
+        public Jugador(string nombre, string numero)
+        {
+            Nombre = nombre;
+            Numero = numero;
+        }
+
+        public Jugador()
+        {
+            Nombre = "Sin asignar";
+            Numero = "Sin asignar";
+        }
+
+    }
+}
---
+++ EquipoBeisbol/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipoBeisbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Equipo> equipos = new List<Equipo>();
+
+            Equipo leones = new Equipo("Leónes");
+            Equipo toros = new Equipo("Toros");
+
+            equipos.Add(leones);
+            equipos.Add(toros);
+
+            Jugador elmer = new Jugador("Elmer", "45");
+            Jugador pablito = new Jugador("Pablito", "12");
+            Jugador bebitamoxita = new Jugador("BebitaMoxita", "05");
+            Jugador ezra = new Jugador("Ezra", "91");
+            Jugador kakaroto = new Jugador("Goku-san", "13");
+
+            toros.Jugadores1.Add(elmer);
+            toros.Jugadores1.Add(bebitamoxita);
+            toros.Jugadores1.Add(ezra);
+        }
+    }
+}
